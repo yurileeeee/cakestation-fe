eac931d6476539eb6363cfd666d9d93c018ec4e8
danbom <bear0369@naver.com>
2022-07-01 21:31:08 +0900

    👕 Fix: eslint 수정 / .prettierrc 삭제 / 메인 index 파일 오류 방지 임시 처리

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,7 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: ["plugin:react/recommended", "airbnb", "prettier"],
+  extends: ["next", "airbnb"],
   parser: "@typescript-eslint/parser",
   parserOptions: {
     ecmaFeatures: {
@@ -25,5 +25,12 @@ module.exports = {
     "react/jsx-one-expression-per-line": "off",
     "implicit-arrow-linebreak": "off",
     "react/jsx-props-no-spreading": "off",
+    "import/no-unresolved": "off",
+    "import/extensions": "off",
+    "no-confusing-arrow": "off",
+    indent: "off",
+    "no-nested-ternary": "off",
+    "object-curly-spacing": "off",
+    "arrow-parens": "off",
   },
 };
